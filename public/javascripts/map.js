a088b4b656a1ca0a2fa3f1ccabea4fede6074860
Anderson Tobon <anderson.tobonp@gmail.com>
2020-11-24 16:30:27 -0500

    Api de bicicletas

--- public/javascripts/map.js
+++ public/javascripts/map.js
@@ -5,5 +5,13 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',{
     
 }).addTo(map);
 
-//'https://{s}.title.openstreetmap.org/{z}/{x}/{y}.png'
-// '&copy;<a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+$.ajax({
+    dataType:"json",
+    url:"api/bicicletas",
+    success: function(result){
+        console.log(result);
+        result.bicicletas.forEach(function(bici){
+            L.marker(bici.ubicacion,{title: bici.id}).addTo(map)
+        });
+    }
+})
